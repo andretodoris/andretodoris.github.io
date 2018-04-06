0a80571e514590e7f6b3ae9812b7b357f28d4f06
andretodoris <38118784+andretodoris@users.noreply.github.com>
2018-04-06 02:47:07 +0200

    Update goo.js

--- goo.js
+++ goo.js
@@ -1,1 +1,2 @@
 function myFunction() { var firstDiv = document.getElementById('watch_dl'); var secondDiv = document.getElementById('goska'); secondDiv.innerHTML =  firstDiv.innerHTML; }
+alert('dodo');
